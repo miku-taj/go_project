0204c8cafc426263d7f53096ea49f1a773b18a87
Mukaddas <tajmukaddas@gmail.com>
2024-07-11 16:46:52 +0500

    dividing hw and lessons

---
+++ lessons/lesson 1/main.go
@@ -0,0 +1,23 @@
+package main
+
+
+var Num int = 1
+
+func main() {
+	println("hello")
+
+	var num, num2 int = 2, 3
+	println(num, num2)
+	num3 := 3
+	println(num3)
+	println(Num)
+
+	var th complex64
+	println(th)
+	const n = 3
+	
+	const (
+		k = 2
+		l = 1
+	)
+}
